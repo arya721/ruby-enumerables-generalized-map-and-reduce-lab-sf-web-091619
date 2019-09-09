cb04779b4f7ebeafff4d8b44b7e8d743aad0d09b
Arya <arya721@gmail.com>
2019-09-09 00:56:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,25 @@
 def map(array)
-new = []
-i = 0
-while i < array.length
-  new.push(yield(array[i]))
-  i += 1
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
 end
-new
+
+
+def reduce(array, sp=nil)
+  if sp
+    total = sp
+    i = 0
+  else
+    total = array[0]
+    i = 1
+  end
+  while i < array.length
+    total = yield(total, array[i])
+    i += 1
+  end
+  total
 end
